0b7283ab064375d064d2df6554281b82a93dc6ea
ajmnz <arnaujim@gmail.com>
2021-08-11 00:47:28 +0200

    add tests

--- test/cursor.test.tsx
+++ test/cursor.test.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import CustomCursor from "../src";
+import { render } from "@testing-library/react";
 
-describe("it", () => {
-	it("renders without crashing", () => {
+// const DEFAULT_PROPS = {
+// 	className: "",
+// 	width: 50,
+// 	height: 50,
+// 	fill: "#000",
+// 	stroke: "#000",
+// 	strokeWidth: 0,
+// };
+
+describe("CustomCursor Component", () => {
+	it("should render without crashing", () => {
 		const div = document.createElement("div");
 		ReactDOM.render(<CustomCursor />, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it("should render correctly", () => {
+		render(<CustomCursor />);
+		expect(document.querySelector(".cursor > svg > circle")).not.toBeNull();
 	});
 });
